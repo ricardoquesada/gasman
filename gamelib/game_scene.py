40747be33e11b48c4ac70150e05e53ccf0553356
Ricardo Quesada <ricardoquesada@gmail.com>
2009-09-01 20:29:42 +0000

    pedoman can eat food

--- gamelib/game_scene.py
+++ gamelib/game_scene.py
@@ -40,7 +40,6 @@ class GameLayer(cocos.layer.Layer):
         self.init_sprites()
 
     def init_sprites( self ):
-
         sprite = Sprite( 'pedoman-character.png' )
         self.add( sprite )
         sprite.position = (200,200)
@@ -49,6 +48,7 @@ class GameLayer(cocos.layer.Layer):
     def init_physics( self ):
         self.points             = []
         self.destroyList        = []
+        self.shitting_places    = []
         settings = fwSettings
 
         # Box2D Initialization
@@ -168,20 +168,21 @@ class GameLayer(cocos.layer.Layer):
         sd.SetAsBox(0.5, 0.5)
         sd.density = 25.0
 
-        friction = [0.75, 0.5, 0.35, 0.1, 0.0]
-
-        for i in range(5):
-            bd=box2d.b2BodyDef() 
-            bd.position = (-15.0 + 4.0 * i, 28.0)
-            body = self.world.CreateBody(bd) 
-
-            sd.friction = friction[i]
-            body.CreateShape(sd)
-            body.SetMassFromShapes()
+        # falling boxes
+#        friction = [0.75, 0.5, 0.35, 0.1, 0.0]
+#
+#        for i in range(5):
+#            bd=box2d.b2BodyDef() 
+#            bd.position = (-15.0 + 4.0 * i, 28.0)
+#            body = self.world.CreateBody(bd) 
+#
+#            sd.friction = friction[i]
+#            body.CreateShape(sd)
+#            body.SetMassFromShapes()
 
         # create Pedo Man
         bd = box2d.b2BodyDef()
-        bd.position = (20,20)
+        bd.position = (18,0)
         bd.angularDamping = 2.0
         bd.linearDamping = 0.1
         body = self.world.CreateBody(bd)
@@ -194,7 +195,33 @@ class GameLayer(cocos.layer.Layer):
         body.SetMassFromShapes()
         self.pedoman_body = body
 
+
+        # shitting places
+        for i in range(10):
+            bd = box2d.b2BodyDef()
+            bd.position = (i*1.5,0)
+            body = self.world.CreateBody(bd)
+            sd = box2d.b2CircleDef()
+            sd.density = 0.0000001
+            sd.radius = 0.5
+            sd.friction = 0.00001
+            sd.restitution = 0.00001
+            body.CreateShape(sd)
+            body.SetMassFromShapes()
+            self.shitting_places.append( body )
+
     def step(self, dt):
+
+        #
+        # check collision detection
+        #
+        self.check_collision_detection()
+        # Reset the collision points
+        self.points = []
+
+        #
+        # Physics main loop
+        #
         # Prepare for simulation. Typically we use a time step of 1/60 of a
         # second (60Hz) and 10 velocity/8 position iterations. This provides a 
         # high quality simulation in most game scenarios.
@@ -202,27 +229,60 @@ class GameLayer(cocos.layer.Layer):
         timeStep = 1.0 / 60
         vel_iters, pos_iters = 10, 8
 
-        # Instruct the world to perform a single step of simulation. It is
-        # generally best to keep the time step and iterations fixed.
-
-#        if self.points:
-#            print self.points
-
-        # Reset the collision points
-        self.points = []
-
+        # It is generally best to keep the time step and iterations fixed.
         self.world.Step(timeStep, vel_iters, pos_iters)
         self.world.Validate()
 
+        #
+        # cleanup
+        #
         # Destroy bodies that have left the world AABB (can be removed if not using pickling)
         for obj in self.destroyList:
             self.world.DestroyBody(obj)
         self.destroyList = []
 
+        #
+        # update sprites based on physics bodies / shapes
+        #
+        self.update_sprite_positions()
+
+    def check_collision_detection( self ):
+
+        # Traverse the contact results. Destroy bodies that
+        # are touching heavier bodies.
+        body_pairs = [(p.shape1.GetBody(), p.shape2.GetBody()) for p in self.points]
+        
+        for body1, body2 in body_pairs:
+            if ( (body1 in self.shitting_places or body2 in self.shitting_places) and \
+                (body1 == self.pedoman_body or body2 == self.pedoman_body ) ):
+                    shape = self.pedoman_body.shapeList[0]
+
+                    # new radius
+                    sd = box2d.b2CircleDef()
+                    sd.density = 1.0
+                    sd.radius = shape.GetRadius() + 0.05
+                    sd.friction = 0.95
+                    sd.restitution = 0.7
+                    self.pedoman_body.CreateShape(sd)
+#                    self.pedoman_body.SetMassFromShapes()
+
+                    # destroy old shape
+                    self.pedoman_body.DestroyShape( shape )
+
+                    # destroy shitting
+                    shit_body = body1
+                    if body2 in self.shitting_places:
+                        shit_body = body2
+                    self.destroyList.append( shit_body )
+                    self.shitting_places.remove( shit_body )
+
+    def update_sprite_positions( self ):
         # position
         position = self.pedoman_body.position
         position = (position.x * PTM_RATIO, position.y * PTM_RATIO)
         self.pedoman_sprite.position = position
+        shape = self.pedoman_body.shapeList[0]
+        self.pedoman_sprite.scale = ( shape.radius * 2)     # scale 1 == 1 meter
 
         # angle
         angle = self.pedoman_body.angle
